50aadff5c88eb6c33f1200edf09f4d83b078f5ce
nmissi-nadia <nmissinadia@gmail.com>
2025-04-26 22:39:02 +0100

    Ajout des élements fillable en signalement model

--- projet/app/Models/Signalement.php
+++ projet/app/Models/Signalement.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Signalement extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'user_id',
+        'type', // offre, profil, commentaire
+        'target_id',
+        'motif',
+        'description',
+        'statut',
+        'traitement_date',
+        'traitement_description'
+    ];
 }
