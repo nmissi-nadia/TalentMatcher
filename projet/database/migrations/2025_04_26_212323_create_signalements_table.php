4042433fef1ba60ca7efc0474dcd393be400a32e
nmissi-nadia <nmissinadia@gmail.com>
2025-04-26 22:26:23 +0100

    Ajout de le migration de controller signalementt

---
+++ projet/database/migrations/2025_04_26_212323_create_signalements_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('signalements', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->string('type')->comment('Type de l\'élément signalé: offre, profil, commentaire');
+            $table->morphs('cible');
+            $table->string('motif')->comment('Raison du signalement');
+            $table->text('description')->nullable()->comment('Détails supplémentaires du signalement');
+            $table->enum('statut', ['pending', 'resolved', 'rejected'])->default('pending');
+            $table->dateTime('traitement_date')->nullable();
+            $table->text('traitement_description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('signalements');
+    }
+};
