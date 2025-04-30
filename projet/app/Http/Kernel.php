9381c94063358773e76936dd8c8a44b826e5d541
nmissi-nadia <nmissinadia@gmail.com>
2025-04-30 10:10:14 +0100

    rectification de fichier kernel

--- projet/app/Http/Kernel.php
+++ projet/app/Http/Kernel.php
@@ -37,6 +37,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \Illuminate\Session\Middleware\StartSession::class,
+            \App\Http\Middleware\VerifyUserStatus::class,
         ],
 
         'api' => [
@@ -66,5 +67,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'check.role' => \App\Http\Middleware\CheckRole::class,
+        'verify.user.status' => \App\Http\Middleware\VerifyUserStatus::class,
     ];
 }
